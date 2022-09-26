a9a557a523c9cfed34bfe1e3fd1bde034f08c47d
D4n-P <soaresdaniel487@gmail.com>
2022-09-25 23:27:14 -0300

    Update dependencyCheck.groovy

--- vars/dependencyCheck.groovy
+++ vars/dependencyCheck.groovy
@@ -1,4 +1,7 @@
 def call() {
+    sh "echo ===== Setup ====="
+    sh "rm -rf dependecy-check || true"
+    sh "rm dependency-check* || true"
     sh "wget https://github.com/jeremylong/DependencyCheck/releases/download/v7.2.1/dependency-check-7.2.1-release.zip"
     unzip zipFile: 'dependency-check-7.2.1-release.zip'
     sh "chmod +x dependency-check/bin/dependency-check.sh && dependency-check/bin/dependency-check.sh -s . -f XML"
