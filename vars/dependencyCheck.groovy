3eedf168b583e1eeb322bb1d051e5bd87590f187
D4n-P <soaresdaniel487@gmail.com>
2022-09-25 23:10:27 -0300

    Update dependencyCheck.groovy

--- vars/dependencyCheck.groovy
+++ vars/dependencyCheck.groovy
@@ -1,5 +1,5 @@
 def call() {
-    sh "wget https://github.com/jeremylong/DependencyCheck/releases/download/v7.2.1/dependency-check-7.2.1-release.zip -O /var/lib/jenkins/workspace/insecure-bank/dependecy-check.zip"
-    unzip zipFile: 'dependency-check.zip', dir: '/var/lib/jenkins/workspace/insecure-bank/'
+    sh "wget https://github.com/jeremylong/DependencyCheck/releases/download/v7.2.1/dependency-check-7.2.1-release.zip"
+    unzip zipFile: 'dependency-check-7.2.1-release.zip', dir: 'dependency-check'
     sh "dependency-check/bin/dependency-check.sh -s . -f xml"
 }
